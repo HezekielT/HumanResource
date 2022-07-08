b66accf6d06edabc719f5e5e8e4458a8d367b3d6
hizkielT <hezekiel.feyissa@gmail.com>
2022-07-08 20:08:13 +0300

    created a module called material to make managing material components easy

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { MatSliderModule } from '@angular/material/slider'
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTableModule } from '@angular/material/table'
 
-const MaterialComponents = [
+const materials = [
   MatSliderModule,
+  MatToolbarModule,
+  MatTableModule
 ]
 
 @NgModule({
-  imports: [ MaterialComponents ],
-  exports: [ MaterialComponents ]
+  imports: [ materials ],
+  exports: [ materials ]
 })
 export class MaterialModule { }
